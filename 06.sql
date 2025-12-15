4644f0634f1faaa8e6745d828dd6b7679ebd2e14
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2025-12-15 10:40:53 +0000

    Add SQL query to find unassigned delivery dates

---
+++ 06.sql
@@ -0,0 +1,23 @@
+WITH
+  all_records AS (
+    SELECT
+      ('2025-12-14'::date + (interval '1' day * s))::date AS dt
+      ,f.*
+    FROM GENERATE_SERIES(0, 11) AS s
+      CROSS JOIN families AS f
+  )
+SELECT
+  ar.dt AS unassigned_date
+  ,ar.family_name AS name
+FROM all_records AS ar
+WHERE
+  NOT EXISTS (
+    SELECT
+      1
+    FROM deliveries_assigned
+    WHERE
+      family_id = ar.id
+      AND gift_date = ar.dt
+  )
+ORDER BY
+  1, 2
